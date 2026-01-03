ada9b38c0aaf9a214c830c3d152b4476df5ff3d0
Jacob <jacoblittler2003@gmail.com>
2026-01-03 10:20:48 -0500

    Outline of methods

---
+++ LinkedList.rb
@@ -0,0 +1,50 @@
+require './Node'
+class LinkedList
+  node = Node.new
+  value = node.value
+  next_node = node.next_node 
+  
+  def initialize
+    @list
+  end
+
+  def append(value)
+    @list << value
+  end
+
+  def prepend(value)
+    @list[0] << value
+  end
+
+  def size
+    
+  end
+
+  def head
+    
+  end
+
+  def tail
+    
+  end
+
+  def at(index)
+
+  end
+
+  def pop
+    
+  end
+
+  def contains(value)
+    
+  end
+
+  def index(value)
+    
+  end
+
+  def to_s
+    
+  end  
+end
